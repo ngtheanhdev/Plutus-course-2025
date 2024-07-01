a2d796f9e220c93ebf48a7e0e9920decb43e1796
Truong Cao Xuan <truongxuanc@gmail.com>
2024-07-01 22:47:37 +0700

    add module7

---
+++ Module07/Off-chain code with Lucid/seed.ts
@@ -0,0 +1,1 @@
+export const secretSeed=" phrase1 phrase2 phrase3 phrase4 phrase5 phrase6 phrase7 phrase8 phrase9 phrase10 phrase11 phrase12 phrase13 phrase14 phrase15 phrase16 phrase17 phrase18 phrase19 phrase20 phrase21 phrase22 phrase23 phrase24 ";
